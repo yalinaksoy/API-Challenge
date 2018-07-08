1b6372a79ae9391dfb7e1eda902d6383d976768b
yalinaksoy <yalinaksoy@gmail.com>
2018-07-08 14:42:38 +0300

    Flask API and running instructions added, I hope it meets all the requirements.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Build a trie from a given input file and sets up a basic flask API for autocomplete
 
 Auto complete API challenge
+
+app.py file should be run with python to start the application
+"python app.py"
+Flask should be installed.
+Can be easily install by command: 'pip install flask'
+ Runs flask API on 127.0.0.1:5000
+ API request format is 127.0.0.1:5000?keyword=<<keyword>>
+Usage: python app.py <<file_path>>
+ Warns if the file does not exist.
+Quit: You can stop the server by ctrl+c combination
--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request, Response
+from trie.trie import Trie
+import json
+import sys
+from os.path import isfile
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    keyword = request.args.get('keyword')
+    return Response(json.dumps(trie.search(keyword)), status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        if isfile(sys.argv[1]):
+            trie = Trie(sys.argv[1])
+        else:
+            print "File not found"
+            exit(1)
+
+    else:
+        print "Autocomplete API."
+        print "Flask should be installed."
+        print "Can be easily install by command: 'pip install flask' "
+        print "\t Runs flask API on 127.0.0.1:5000"
+        print "\t API request format is 127.0.0.1:5000?keyword=<<keyword>>"
+        print "Usage: python app.py <<file_path>>"
+        print "\t Warns if the file does not exist."
+        print "Quit: You can stop the server by ctrl+c combination"
+        exit(1)
     app.run(debug=True)
